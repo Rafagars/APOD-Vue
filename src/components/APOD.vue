ec921c21651d84b2a9b99c4c46b6057668dc7dc0
rafagars <rafagar96@gmail.com>
2020-12-25 19:48:11 -0400

    Design changes

--- src/components/APOD.vue
+++ src/components/APOD.vue
@@ -1,28 +1,27 @@
 <template>
-    <div class="container text-center" v-if="result">
+    <div class=" text-center" v-if="result">
 		<h1> Astronomy Picture of the Day </h1>
 		<p>Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer. </p>
         <h2>{{result.title}}</h2>
         <h5>{{result.date}}</h5>
-        <div v-if="result.media_type === 'image'">
-            <a :href=result.url >
-                <img :src=result.url :alt=result.title >
-            </a>
-        </div>
-        <div v-else-if="result.media_type === 'video'" class="embed-responsive embed-responsive-16by9">
-			<iframe :title=result.title class="embed-responsive-item" :src=result.url ></iframe>
-		</div>
-        <div v-else>
-            <a :href=result.apod_site > Click Here </a>
-        </div>
+        <div class="flex">
+            <button id="prev" class="btn bg-transparent col-flex" @click="prev()"> &lt; </button>
+            <div v-if="result.media_type === 'image'" class="col-flex">
+                <a :href=result.url >
+                    <img :src=result.url :alt=result.title >
+                </a>
+            </div>
+            <div v-else-if="result.media_type === 'video'" class="embed-responsive embed-responsive-16by9 col-flex">
+                <iframe :title=result.title class="embed-responsive-item" :src=result.url ></iframe>
+            </div>
+            <div v-else class="col-flex">
+                <a :href=result.apod_site > Click Here </a>
+            </div>
+            <button id="next" class="btn bg-transparent col-flex" @click="next()"> &gt; </button>
+        </div> 
         <h6>{{result.copyright}}</h6>
         <br>
-        <p>{{result.description}}</p>
-        <nav class="text-center">
-            <button class="btn btn-dark" @click="prev()">Prev</button>
-            <router-link to="/archives">Archives</router-link>
-            <button class="btn btn-dark" @click="next()">Next</button>
-        </nav>
+        <p class="container">{{result.description}}</p>
     </div>
 </template>
 
@@ -80,8 +79,14 @@ export default {
 img{
     width: 85%;
 }
-button{
-    margin-left: 5px ;
-    margin-right: 5px ;
+.flex{
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
 }
+.col-flex{
+    font-size: 2em;
+    color: white;
+}
+
 </style>
